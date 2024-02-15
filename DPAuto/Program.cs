eea865115669224dea50ae7f340c1246005cadfa
Leonardo Chuello <ljchuello@gmail.com>
2024-02-15 17:05:46 -0500

    Agregar archivos de proyecto.

---
+++ DPAuto/Program.cs
@@ -0,0 +1,12 @@
+﻿using Libreria;
+
+namespace DPAuto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Print("abc", LogType.Warning);
+        }
+    }
+}
